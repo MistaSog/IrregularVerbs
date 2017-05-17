b3aa99d2a872e3b51652a41c207ba8ea75c93f53
MistaSog <mistersog@gmail.com>
2017-05-17 18:04:38 +0700

    Complete ReadDictionary function

--- src/main.c
+++ src/main.c
@@ -3,7 +3,11 @@
 
 int main()
 {
+	Record A[VERBS_NUMBER];
 	printf("IrregularVerbs\n");
-	(ReadDictionary()?printf("Done!\n"):printf("Error!\n"));
+	(ReadDictionary("src/Dictionary", A)?printf("Done!\n"):printf("Error!\n"));
+	for(int i=0; i<VERBS_NUMBER; i++){
+		printf("%s %s %s\n", A[i].inf, A[i].past_simple, A[i].past_participle);
+	}
 	return 0;
 }
