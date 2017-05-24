797e0bb4ef9dc9c8f36c3ed38c8cec9c12edfa2d
MistaSog <mistersog@gmail.com>
2017-05-25 00:32:50 +0700

    Hotfix for printing

--- src/verbs.c
+++ src/verbs.c
@@ -18,13 +18,14 @@ int ReadDictionary(char fname[],Record *A)
 
 int PrintDictionary(Record *A)
 {	
+	int i=0;
 	if (strcmp(A[0].inf,"NULL")==0) {
 		return 0;
 	}
 	printf("|====================+====================+====================|\n");
 	printf("|     Infinitive     |     Past Simple    |   Past Participle  |\n");
 	printf("|====================+====================+====================|\n");
-	for (int i=0; i<VERBS_NUMBER; i++) {
+	for (i=0; i<VERBS_NUMBER; i++) {
 		printf("| %18s | %18s | %18s |\n", A[i].inf, A[i].past_simple, A[i].past_participle);
 	}
 	printf("|====================+====================+====================|\n");
