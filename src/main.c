b3e77786dc54307746a0083ca998056d4659595f
MistaSog <mistersog@gmail.com>
2017-05-25 00:27:50 +0700

    Implement dictionary output

--- src/main.c
+++ src/main.c
@@ -6,8 +6,6 @@ int main()
 	Record A[VERBS_NUMBER];
 	printf("IrregularVerbs\n");
 	ReadDictionary("src/Dictionary", A) ? printf("Done!\n") : printf("Error!\n");
-	for (int i=0; i<VERBS_NUMBER; i++) {
-		printf("%s %s %s\n", A[i].inf, A[i].past_simple, A[i].past_participle);
-	}
+	PrintDictionary(A) ? printf("Done!\n") : printf("Error!\n");
 	return 0;
 }
--- src/verbs.c
+++ src/verbs.c
@@ -15,3 +15,18 @@ int ReadDictionary(char fname[],Record *A)
 	}
 	return 1;
 }
+
+int PrintDictionary(Record *A)
+{	
+	if (strcmp(A[0].inf,"NULL")==0) {
+		return 0;
+	}
+	printf("|====================+====================+====================|\n");
+	printf("|     Infinitive     |     Past Simple    |   Past Participle  |\n");
+	printf("|====================+====================+====================|\n");
+	for (int i=0; i<VERBS_NUMBER; i++) {
+		printf("| %18s | %18s | %18s |\n", A[i].inf, A[i].past_simple, A[i].past_participle);
+	}
+	printf("|====================+====================+====================|\n");
+	return 1;
+}
