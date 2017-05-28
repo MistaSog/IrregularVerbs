19ca2df143d67363c149968f40c3b840fbe7ae15
vvlvdyvboy <ihatenetmail@gmail.com>
2017-05-29 02:40:09 +0700

    Tests for marks

--- test/verbs_test.c
+++ test/verbs_test.c
@@ -33,3 +33,81 @@ CTEST(output_suite, error_test)
 	const int expected = 0;
 	ASSERT_EQUAL(expected, result);
 }
+
+const int ALL=100;
+
+CTEST(mark_suite, below_zero_test)
+{
+	const int result = TaskMark(ALL, -1);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, zero_test)
+{
+	const int result = TaskMark(ALL, 0);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_two_left_test)
+{
+	const int result = TaskMark(ALL, 1);
+	const int expected = 2;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_two_right_test)
+{
+	const int result = TaskMark(ALL, 35);
+	const int expected = 2;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_three_left_test)
+{
+	const int result = TaskMark(ALL, 36);
+	const int expected = 3;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_three_right_test)
+{
+	const int result = TaskMark(ALL, 60);
+	const int expected = 3;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_four_left_test)
+{
+	const int result = TaskMark(ALL, 61);
+	const int expected = 4;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_four_right_test)
+{
+	const int result = TaskMark(ALL, 85);
+	const int expected = 4;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_five_left_test)
+{
+	const int result = TaskMark(ALL, 86);
+	const int expected = 5;
+	ASSERT_EQUAL(expected, result);
+}
+
+CTEST(mark_suite, mark_five_right_test)
+{
+	const int result = TaskMark(ALL, 100);
+	const int expected = 5;
+	ASSERT_EQUAL(expected, result);
+}
+CTEST(mark_suite, over_100_test)
+{
+	const int result = TaskMark(ALL, 101);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
