1a09b7dacd7645dddae2f0bebd1fcd4a5191cff4
MistaSog <mistersog@gmail.com>
2017-05-29 04:38:27 +0700

    80 columns in main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@ int main()
 {
 	Record A[VERBS_NUMBER];
 	printf("IrregularVerbs\n");
-	read_dictionary(DICTIONARY_PATH, A) ? printf("Done!\n") : printf("Error!\n");
+	read_dictionary(DICTIONARY_PATH, A) ?
+		printf("Done!\n") : printf("Error!\n");
 	print_dictionary(A);
 	print_task(A);
 	return 0;
